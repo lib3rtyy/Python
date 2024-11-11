22c939176e28c9af5172e045c752ddda45ef9d4e
Eleftheria Poulianiti <116922036+lib3rtyy@users.noreply.github.com>
2024-11-11 19:31:31 +0200

    Add files via upload

---
+++ Beginner/hangman.py
@@ -0,0 +1,29 @@
+import random
+
+words = [ 'shadow', 'river', 'bliss', 'galaxy', 'puzzle', 'flame', 'wisdom', 'echo', 'valor', 'crystal', 'whisper', 'oasis', 'jungle', 'thunder', 'horizon', 'riddle', 'spark', 'fable', 'mystic', 'ember']
+chosen_word = random.choice(words)
+word_display = [ '_' for _ in chosen_word]
+attempts = 10
+
+print ("Hangman    Game")
+
+while attempts>0 and '_' in word_display:
+    print("\n" + ' '.join(word_display))
+    guess = input("Guess a letter:").lower()
+
+    if len(guess) != 1 or not guess or not guess.isalpha():
+        print("Please enter a single valid letter.")
+        continue
+
+    if guess in chosen_word:
+        for index, letter in enumerate(chosen_word):
+            if letter == guess:
+                word_display[index] = guess
+    else:
+        print(f"No letter '{guess}' in the word.")
+        attempts -=1
+
+if '_' not in word_display:
+    print("You guessed the word! The word was: " +chosen_word)
+else:
+    print ("You failed! The word was: "+chosen_word)
